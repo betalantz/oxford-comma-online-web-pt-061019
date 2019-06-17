80994d5154d0cf2f5292a30a55f3e95256286205
Lantz Warrick <betalantz@gmail.com>
2019-06-17 05:40:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  last = array[array.last]
+  case array
+    when array.size == 1
+      return array.join(", ")
+    when array.size == 2
+      array.pop
+  end
 end
+
+puts oxford_comma(["kiwi"])
