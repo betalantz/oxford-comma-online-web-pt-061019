8101f65402731924896ce81d3b795c4d301bb504
Lantz Warrick <betalantz@gmail.com>
2019-06-17 05:42:48 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   last = array.last
   case array
     when array.size == 1
-      return array.join(", ")
+      return last
     when array.size == 2
       array.pop
   end
