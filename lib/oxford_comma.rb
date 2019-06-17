825a6c031d5caf7f27460d89cc7c3336af4ffe67
Lantz Warrick <betalantz@gmail.com>
2019-06-17 05:54:32 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,18 +1,18 @@
 def oxford_comma(array)
   last = array.last
   case
-  when array.size == 1
-    last
-  when array.size == 2
-    array.pop
-    result = array.join
-    result << " and #{last}"
-    result
-  when array.size > 2
-    array.pop
-    result = array.join(", ")
-    result << "and #{last}"
-    result
+    when array.size == 1
+      last
+    when array.size == 2
+      array.pop
+      result = array.join
+      result << " and #{last}"
+      result
+    when array.size > 2
+      array.pop
+      result = array.join(", ")
+      result << "and #{last}"
+      result
   end
 end
 
