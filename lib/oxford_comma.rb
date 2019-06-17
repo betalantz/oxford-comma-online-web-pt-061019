10c565ac316cfe18029c8c273f78f2dfbfec686c
Lantz Warrick <betalantz@gmail.com>
2019-06-17 05:57:18 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  last = array.last
+  case
+    when array.size == 1
+      return last
+    when array.size == 2
+      array.pop
+      result = array.join
+      result << " and #{last}"
+      return result
+    when array.size > 2
+      array.pop
+      result = array.join(", ")
+      result << ", and #{last}"
+      return result
+  end
 end
+
+puts oxford_comma(["kiwi", "apple", "peach"])
