e9f1db975a2e33fdd29854a33dffd49b25133910
Lantz Warrick <betalantz@gmail.com>
2019-06-17 05:43:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,10 @@
 def oxford_comma(array)
   last = array.last
   case array
-    when array.size == 1
-      return last
-    when array.size == 2
-      array.pop
+  when array.size == 1
+    return last
+  when array.size == 2
+    array.pop
   end
 end
 
